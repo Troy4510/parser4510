8f8a3146bf6df707d4f1d5d961f2a375ac6127cb
troy4510 <iceman.cybercore@gmail.com>
2024-01-28 17:39:20 +0200

    created worked 1-page variant

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
+
 
 def parser(url:str):
     res = requests.get(url=url)
-    soup = BeautifulSoup(res.text, "lxml")
-    #products = soup.find_all("div",class_="p__bottom")
-    products = soup.find_all(class_="p__title")
-    print (f'найдено:{len(products)}')
-    for product in products:
-        name = product.get("title")
-        #price = product.find 
+    if res.status_code == 200:
+        print('страница доступна')
+    soup = BeautifulSoup(res.text, 'lxml')
+    #products = soup.find_all('div', class_='p__bottom')
+    products_names = soup.find_all('a', class_='p__title')
+    products_prices = soup.find_all('div',class_='p__price p__price--old')
+    
+    #print (f'найдено products:{len(products)}')
+    print (f'найдено names:{len(products_names)}')
+    print (f'найдено prices:{len(products_prices)}')
+    
+    for i in range(len(products_names)):
+         n1 = products_names[i].text
+         n1 = n1.strip()
+         p1 = products_prices[i].text
+         p1 = p1.strip()
+         print(f'№{i+1}. {n1} {p1}')
+         
+    #for price in products_prices:
+        #price = price.text
+        #price = price.strip() #уд пробелы в начале и в конце
+        #или price = price.replace(' ', '')
+        #print(price)
+
+    #for name in products_names:
+    #    print (name.text)
         
-        print(name, price)
-
-def create_csv():
-    pass
-
-def write_csv():
-    pass
+    #for product in products:
+    #    print(product)
+       
 
 if __name__ == "__main__":
     parser(url="https://tamaris.ru/catalog/obuv/")
