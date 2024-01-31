1b1dbded618f24a00481fe3f9fd8f787ab29ecbc
troy4510 <iceman.cybercore@gmail.com>
2024-01-31 21:50:21 +0200

    begin to make sql operations. test flow base from parser

--- main.py
+++ main.py
@@ -4,15 +4,13 @@ import datetime
 import time
 import sql_module as sm
 
-folder = './parser4510/'
+main_folder = './parser4510/'
 
-#sm.sm_test()
-#sm.create_base(way1=folder)
-
-def parser(url:str):
+def parser(url):
     res = requests.get(url=url)
     soup = BeautifulSoup(res.text, 'lxml')
     products = soup.find_all('div', class_='p__bottom')
+    print(f'products found: {len(products)}')
     
     for product in products:
         name1 = product.find('a', class_='p__title').get('title')
@@ -24,19 +22,20 @@ def parser(url:str):
         price1 = price1.replace('р.', '')#удаляем надпись "рублей"
         price1 = price1.replace(' ', '')#удаляем ненужные пробелы
         price1 = int(price1)#конвертируем цену из текста в число, тип переменной теперь int
-        print(f'{name1} {price1} {link1}')
+        print(f'[add to sql_base] {name1} {price1}')
+        sm.add_product(main_folder, name1, link1, price1)
  
     
 
 def make_url_list(url):#начинаем с начального адреса и проверяем доступность страниц
-    url_list = open('./parser4510/url_list.txt', '+w')
+    url_list = open(f'{main_folder}url_list.txt', '+w')
     url_list.write(url+'\n')
     #?PAGEN_1=2,?PAGEN_1=3... 
     
-    for i in range(2,23):    
+    for i in range(2,25):    
         urlx = url + '?PAGEN_1=' + str(i)
         if requests.get(urlx).status_code == 200:
-            print(urlx + '\n')
+            print(urlx)
             url_list.write(urlx+'\n')
 
     url_list.close
@@ -47,6 +46,8 @@ def make_url_list(url):#начинаем с начального адреса и
 
 
 if __name__ == "__main__":
+    print('[RUN MAIN]')
+    sm.check_base(db_folder=main_folder)
+    #sm.add_product(main_folder, 'BOOTS', 'llliinnnkkk', 2000)
     #make_url_list(url="https://tamaris.ru/catalog/obuv/")
     parser(url="https://tamaris.ru/catalog/obuv/")
-    #pass
--- sql_module.py
+++ sql_module.py
@@ -1,9 +1,37 @@
 import sqlite3
 
-def create_base(way1:str):
-    connection = sqlite3.connect(way1 + 'tamaris.db')
+def check_base(db_folder:str):
+    print('[sm.check_base]')
+    connection = sqlite3.connect(db_folder + 'sql_base.db')
+    cursor = connection.cursor()
+    
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS tProduct (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    link TEXT,
+    price INTEGER,
+    sverka TEXT
+    )
+    ''')
+    
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS tStat (
+    date_upd TEXT,
+    vsego INTEGER,
+    insert_product INTEGER,
+    delete_product INTEGER
+    ) 
+    ''')
+    
+    connection.commit()
     connection.close()
-    print('base create complete')
-
-def sm_test():
-    print('test_ok')
+    
+    
+def add_product(db_folder:str, name:str, link:str, price:int):
+    connection = sqlite3.connect(db_folder + 'sql_base.db')
+    cursor = connection.cursor()
+    query = 'INSERT INTO tProduct (name, link, price, sverka) VALUES (?, ?, ?, ?)'
+    cursor.execute(query, (name, link, price, 'ok',))
+    connection.commit()
+    connection.close()
