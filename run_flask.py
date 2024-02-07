b0bf1927217f5fba4e6a667ea4b9c320f7ae8373
troy4510 <iceman.cybercore@gmail.com>
2024-02-07 20:08:28 +0200

    flask deploy

--- run_flask.py
+++ run_flask.py
@@ -7,10 +7,10 @@ app = Flask(__name__, template_folder = 'app/templates')
 @app.route('/')
 @app.route('/index.html')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', data1 = 'one111', data2 = 'two222', data3 = 'three333')
 
 @app.route('/2')
 def page_two():
     return 'page_two'
 
-app.run(host='127.0.0.1', port=5000, debug=True)
+app.run(host='1c.k-desk.ru', port=88, debug=True)
