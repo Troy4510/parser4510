1248cfb680078c4776d5187ef162e4d76e6a12fa
troy4510 <iceman.cybercore@gmail.com>
2024-02-24 17:12:23 +0200

    test release

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ pages_count = 22
 makelinks = True
 check_sql_base = True
 zero_launch_check = True
+parse_today = False
 
 def parser_1page(url): #парсит одну страницу по ссылке и заносит товары в базу
     res = requests.get(url=url)
@@ -36,9 +37,8 @@ def parser_1page(url): #парсит одну страницу по ссылке
 
 def make_url_list(url):#начинаем с начального адреса и проверяем доступность страниц
     url_list = open(f'{main_folder}url_list.txt', '+w')
-    print('[MAIN: make_url_list init]')
-    #url_list.write(url+'\n')
-    #?PAGEN_1=2,?PAGEN_1=3... 
+    print(f'{datetime.datetime.now()} [MAIN: make_url_list init]')
+
     for i in range(1,pages_count+1): 
         urlx = url + '?PAGEN_1=' + str(i)
         
@@ -47,11 +47,11 @@ def make_url_list(url):#начинаем с начального адреса и
             url_list.write(urlx+'\n')
             
     url_list.close
-    print('[MAIN: make_url_list complete]')
+    print(f'{datetime.datetime.now()} [MAIN: make_url_list complete]')
 
 
 def multiparce():
-    print('[MAIN: multiparce start]')
+    print(f'{datetime.datetime.now()} [MAIN: multiparce start]')
     work_url_list = main_folder + 'url_list.txt'
     workfile = open(work_url_list, '+r')
     
@@ -61,26 +61,26 @@ def multiparce():
         parser_1page(link1)#постраничный запуск парсера (построчно из url-файла)
         
     workfile.close()
-    print('[MAIN: multiparce end]')
+    print(f'{datetime.datetime.now()} [MAIN: multiparce end]')
 
     
 
 if __name__ == "__main__":
-    print('[RUN MAIN]')
+    print(f'{datetime.datetime.now()} [RUN MAIN]')
     
     #проверяет наличие базы и если её нет - создаёт
     if os.path.exists(main_folder + 'sql_base.db') == False and check_sql_base == True: 
         sm.check_base(db_folder=main_folder)
-        print('[MAIN: sql_base created]')
+        print(f'{datetime.datetime.now()} [MAIN: sql_base created]')
     
     #создаёт url-список для проверки в виде файла
     if os.path.exists(main_folder + 'url_list.txt') == False and makelinks == True: 
         make_url_list(url="https://tamaris.ru/catalog/obuv/")
-        print('[MAIN: url_list created]')
+        print(f'{datetime.datetime.now()} [MAIN: url_list created]')
     
     #запускает первую процедуру парсинга если база пустая
     if sm.counter(main_folder) == None and zero_launch_check == True:
-        print('[MAIN: zero_start init]')
+        print(f'{datetime.datetime.now()} [MAIN: zero_start init]')
         multiparce()
         #и делает первую запись в tStat
         date_upd = datetime.datetime.now()
@@ -90,11 +90,33 @@ if __name__ == "__main__":
         sm.write_stat(main_folder, date_upd, vsego, insert_product, delete_product)
         sm.added_products = 0
         sm.deleted_products = 0
-        print('[MAIN: zero_start complete]')
+        print(f'{datetime.datetime.now()} [MAIN: zero_start complete]')
     
-    print('[MAIN: check complete]')
-    #while True:
+    print(f'{datetime.datetime.now()} [MAIN: check complete]')
     
+    #основной цикл
+    time_now = time.time()
+    time_next = time_now + 100
+    print(f'{datetime.datetime.now()} [MAIN: start core]')
+    
+    while True:
+        if time.time() >= time_next:            #когда сработал будильник
+            sm.added_products = 0               #обнуляется счётчик добавленных
+            sm.deleted_products = 0             #обнуляется счётчик удалённых
+            sm.erase_sverka(main_folder)        #все записи базы меняют статус на 'need_check'
+            multiparce()                        #запускается парсер, счётчики и статусы проверенных на 'ok'
+            sm.erase_unchecked(main_folder)     #удаляются товары которых нет на сайте (статус 'need_check' не сменился на 'ok')
+            date_upd = datetime.datetime.now()  #дата для внесения в таблицу статистики
+            vsego = sm.counter(main_folder)     #количество записей для таблицы статистики
+            insert_product = sm.added_products  #количество добавленных
+            delete_product = sm.deleted_products    #количество удалённых
+            sm.write_stat(main_folder, date_upd, vsego, insert_product, delete_product) #запись в таблицу статистики
+            time_now = time.time()              #сбрасывается отсчёт времени
+            time_next = time_now + 86400        #ставится будильник на сутки вперед
+        else:                                   #иначе раз в полчаса и делать запись об ожидании и дальше спать
+            print(f'{datetime.datetime.now()} [MAIN: core wait ok...]')
+            time.sleep(1800)
+    #не забыть! дистанционный запуск на сервере всегда делать через nohup чтоб работало в фоне и оставались логи консоли
     
     
     #всякие пробы))) прим.: час - 3600 сек, сутки - 86400 сек
--- run_flask.py
+++ run_flask.py
@@ -3,14 +3,16 @@ from flask import request
 from flask import render_template
 import sql_module as sm
 
-app = Flask(__name__, template_folder = 'app/templates')
+app = Flask(__name__, template_folder = 'app/templates', static_folder = 'app/static')
 main_folder = './parser4510/'
 
 
 @app.route('/')
 @app.route('/index.html')
 def index():
-    return render_template('index.html', data1 = 'one111', data2 = 'two222', data3 = 'three333')
+    stat_value = sm.read_stat(main_folder)
+    product_value = create_table_products(1,50)
+    return render_template('index.html', stat = stat_value, products = product_value)
 
 @app.route('/products')
 @app.route('/products.html')
@@ -22,7 +24,11 @@ def products():
 @app.route('/stat')
 @app.route('/stat.html')
 def create_table_stat():
-    value = create_table_stat()
+    value = sm.read_stat(main_folder)
+    print(type(value))
+    print(type(value[0]))
+    print(value[0][0])
+    print(value[0][1])
     return render_template('stat.html', value=value)
 
 
